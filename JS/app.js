460ad8010eced021449621148f59de687c14862d
Gerardo Rodriguez <gdrodriguezok@gmail.com>
2022-04-19 01:20:46 -0300

    +accesorios+relojes

--- JS/app.js
+++ JS/app.js
@@ -1,7 +1,11 @@
 //Selects
-const productosDOM = document.querySelector(".productos-centro") || '';
-const matesDOM = document.querySelector(".mates-centro") || '';
-const lentesDOM = document.querySelector(".lentes-centro") || '';
+const productosDOM = document.querySelector(".productos-centro");
+const setMatesDOM = document.querySelector("#setMates") || '';
+const matesDOM = document.querySelector("#mates") || '';
+const lentesDOM = document.querySelector("#lentes") || '';
+const accesoriosDOM = document.querySelector("#accesorios") || '';
+const relojesDOM = document.querySelector("#relojes") || '';
+
 
 
 
@@ -35,7 +39,7 @@ const mostrarSets = (sets) => {
     ` 
     });
 
-    productosDOM.innerHTML = resultado;
+    setMatesDOM.innerHTML = resultado;
 }
 
 
@@ -106,12 +110,87 @@ const mostrarLentes = (lentes) => {
     lentesDOM.innerHTML = resultado;
 }
 
+// ----------- MAPEO DE ACCESORIOS -----------
+//obtener los accesorios
+const getAccesorios = async () => {
+    try{
+        let resultado = await fetch('./db/accesorios.json');
+        let data = await resultado.json();
+        return data;
+    }catch(error){
+        console.log(error);
+    }
+}
+
+//mostrar los accesorios
+const mostrarAccesorios = (accesorios) => {
+    let resultado = '';
+
+    accesorios.forEach(accesorio => {
+        resultado += `
+        <!-- Producto -->
+        <article class="producto">
+            <div class="img-container">
+                <img src=${accesorio.imgSrc} alt="${accesorio.nombre} ${accesorio.tipo}" class="producto-img">
+            </div>
+            <h3 class="producto-nombre1">${accesorio.nombre}</h3>
+            <h3 class="producto-nombre2">${accesorio.tipo}</h3>
+        </article>
+        <!-- Fin producto -->
+    ` 
+    });
+
+    accesoriosDOM.innerHTML = resultado;
+}
+
+// ----------- MAPEO DE RELOJES -----------
+//obtener los relojes
+const getRelojes = async () => {
+    try{
+        let resultado = await fetch('./db/relojes.json');
+        let data = await resultado.json();
+        return data;
+    }catch(error){
+        console.log(error);
+    }
+}
+
+//mostrar los relojes
+const mostrarRelojes = (relojes) => {
+    let resultado = '';
+
+    relojes.forEach(reloj => {
+        resultado += `
+        <!-- Producto -->
+        <article class="producto">
+            <div class="img-container">
+                <img src=${reloj.imgSrc} alt="${reloj.tipo}" class="producto-img">
+            </div>
+            <div>
+                ${Array.isArray(reloj.nombre) ? reloj.nombre.map( nom => {
+                    return `<h3 class="item-multiple">${nom}</h3>`
+                }) : `<h3 class="producto-nombre1">${reloj.nombre}</h3>`}
+            <h3 class="producto-nombre2">${reloj.tipo}</h3>
+        </article>
+        <!-- Fin producto -->
+    ` 
+    });
+
+    relojesDOM.innerHTML = resultado;
+}
+
 
 //llamada condicional de las funciones
-if (productosDOM !== ''){
+if (setMatesDOM !== ''){
     getSets().then(sets => mostrarSets(sets))
 }else if(matesDOM !== ''){
     getMates().then(mates => mostrarMates(mates))
-}else{
+}else if(lentesDOM !== ''){
     getLentes().then(lentes => mostrarLentes(lentes))
+} else if (accesoriosDOM !== ''){
+    getAccesorios().then(accesorios => mostrarAccesorios(accesorios))
+} else if (relojesDOM !== ''){
+    getRelojes().then(relojes => mostrarRelojes(relojes))
+} else {
+    productosDOM.innerHTML = 'No hay elementos para mostrar';
 }
